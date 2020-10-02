8776a48771fff687e37d875106f8f8acf35defcc
Reza <rezamohideen@utexas.edu>
2020-10-01 19:43:11 -0500

    bfs done

--- P3/src/assignment3/Main.java
+++ P3/src/assignment3/Main.java
@@ -14,16 +14,23 @@
 
 
 package assignment3;
+import javax.xml.soap.Node;
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
     // static variables and constants only here.
+    public static Set<String> dictionary;       //all words of provided dictionary
+    public static ArrayList<String> wordList;   //user inputs
+    public static String startingWord;          //starting word of word ladder
+    public static String endingWord;            //ending word of world ladder
+    public static Scanner kb;                   //input scanner
+    public static Hashtable<String, List<String>> comboDict;
 
     public static void main(String[] args) throws Exception {
 
-        Scanner kb;	// input Scanner for commands
+        //Scanner kb;	// input Scanner for commands
         PrintStream ps;	// output file, for student testing and grading only
         // If arguments are specified, read/write from/to files instead of Std IO.
         if (args.length != 0) {
@@ -37,12 +44,25 @@ public class Main {
         initialize();
 
         // TODO methods to read in words, output ladder
+        ArrayList<String> wordLadder = getWordLadderBFS(startingWord,endingWord);
+        printLadder(wordLadder);
     }
 
     public static void initialize() {
         // initialize your static variables or constants here.
         // We will call this method before running our JUNIT tests.  So call it
         // only once at the start of main.
+        dictionary = makeDictionary();
+        wordList = parse(kb);
+        if (wordList.get(0) == "/quit"){
+            return;
+        }
+        else {
+            startingWord = wordList.get(0).toString();
+            endingWord = wordList.get(1).toString();
+        }
+
+        comboDict = createComboDict(dictionary);
     }
 
     /**
@@ -51,8 +71,18 @@ public class Main {
      * If command is /quit, return empty ArrayList.
      */
     public static ArrayList<String> parse(Scanner keyboard) {
-        // TO DO
-        return null;
+        // obtain user input for start and end word
+        String userInput = keyboard.nextLine();
+
+        // check if /quit was entered
+        if (userInput.equals("/quit")) {
+            return new ArrayList<String>(Arrays.asList("/quit","/quit"));
+        }
+
+        ArrayList<String> wordList = new ArrayList<String>(Arrays.asList(userInput.split(" ")));
+
+
+        return wordList;
     }
 
     public static ArrayList<String> getWordLadderDFS(String start, String end) {
@@ -64,17 +94,108 @@ public class Main {
         return null; // replace this line later with real return
     }
 
+    public static class WordNode {
+        public String word; public int depth; public String prev;
+
+        public WordNode(String word, int depth, String prev) {
+            this.word = word; this.depth = depth; this.prev = prev;
+        }
+    }
+
     public static ArrayList<String> getWordLadderBFS(String start, String end) {
 
-        // TODO some code
+        ArrayList<String> ladder = new ArrayList<String>();
+        ArrayList<String> discoveredStrings = new ArrayList<String>();
+        ArrayList<WordNode> discoveredNodes = new ArrayList<>();
 
-        return null; // replace this line later with real return
+        Queue<WordNode> Q = new LinkedList<>();
+
+        int level = 0;
+        WordNode startNode = new WordNode(start.toUpperCase(), level, null);
+        Q.add(startNode);
+        discoveredStrings.add(startNode.word);
+        discoveredNodes.add(new WordNode(startNode.word, startNode.depth, startNode.prev));
+
+        while(!Q.isEmpty()) {
+            WordNode word = Q.remove();
+
+            for (int i = 0; i < word.word.length(); i++) {
+                // replace single character with "*"
+                String starWord = word.word.substring(0, i) + "*" + word.word.substring(i + 1);
+
+                // get value of dictionary for given key
+                List<String> adjacentWords = comboDict.get(starWord);
+                adjacentWords = new ArrayList<>(adjacentWords);
+
+                // if ending word is found
+                if (adjacentWords.contains(end.toUpperCase())) {
+                    discoveredStrings.add(end.toUpperCase());
+                    discoveredNodes.add(new WordNode(end.toUpperCase(), level+1, word.word));
+                    Q.clear();
+                    break;
+                }
+
+                // add all adjacent words that havent been discovered to queue
+                for (String adjWord : adjacentWords) {
+                    if (!discoveredStrings.contains(adjWord)) {
+                        Q.add(new WordNode(adjWord, level+1,word.word));
+                        discoveredStrings.add(adjWord);
+                        discoveredNodes.add(new WordNode(adjWord, word.depth, word.word));
+                    }
+                }
+            }
+            level++;
+        }
+
+        if (discoveredStrings.contains(end.toUpperCase())) {
+            ladder = backtrackBFS(discoveredNodes);
+            return ladder; // replace this line later with
+        }
+        else {
+            return ladder;
+        }
     }
 
+    public static ArrayList<String> backtrackBFS(ArrayList<WordNode> discoveredNodes) {
+        ArrayList<String> ladder = new ArrayList<>();
+
+        int index = discoveredNodes.size() - 1;
+        String prevWord = "****";
+
+        while(prevWord != null) {
+            ladder.add(discoveredNodes.get(index).word);
+            prevWord = discoveredNodes.get(index).prev;
+            for (int i = 0; i < discoveredNodes.size(); i++) {
+                if (discoveredNodes.get(i).word == prevWord) {
+                    index = i;
+                }
+            }
+        }
+        return ladder;
+    }
+
+    public static int calcDistance(String adjWord, String endWord) {
+        int distance = 0;
+        for (int i = 0; i < adjWord.length(); i++) {
+            distance += endWord.toUpperCase().charAt(i) - adjWord.charAt(i);
+        }
+
+        return distance;
+    }
 
     public static void printLadder(ArrayList<String> ladder) {
-
+        if (ladder.size() > 0) {
+            Collections.reverse(ladder);
+            System.out.println("a " + (ladder.size() - 2) + "-rung word ladder exists between " + startingWord + " and " + endingWord);
+            for (String word : ladder) {
+                System.out.println(word.toLowerCase());
+            }
+        }
+        else {
+            System.out.println("no word ladder was found between " + startingWord + " and " + endingWord);
+        }
     }
+
     // TODO
     // Other private static methods here
 
@@ -94,5 +215,40 @@ public class Main {
             words.add(infile.next().toUpperCase());
         }
         return words;
+    }
+
+    // create dictionary that holds words and their adjacent words in a list
+    public static Hashtable<String, List<String>> createComboDict(Set<String> wordList) {
+        Hashtable<String, List<String>> comboDict = new Hashtable<String, List<String>>();
+
+        // iterate over every word in wordList
+        for (String word : wordList) {
+            // iterate over each character in word
+            for (int i = 0; i < word.length(); i++){
+                // replace single character with "*"
+                String starWord = word.substring(0,i) + "*" + word.substring(i+1);
+
+                // if dictionary is empty or key does not exist add key,value
+                if (comboDict.isEmpty() == true || comboDict.get(starWord) == null) {
+                    List<String> temp = Arrays.asList(new String[]{word});
+                    comboDict.put(starWord, temp);
+                }
+                else {
+                    // get value of dictionary for given key
+                    List<String> adjacentWords = comboDict.get(starWord);
+                    adjacentWords = new ArrayList<>(adjacentWords);
+
+
+                    // add word to list if it doesn't already exist
+                    if (!adjacentWords.contains(word)) {
+                        adjacentWords.add(word);
+                        comboDict.put(starWord,adjacentWords);
+
+                    }
+                }
+            }
+        }
+
+        return comboDict;
     }
 }
