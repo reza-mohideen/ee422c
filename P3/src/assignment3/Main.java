940f6753f20cf51a816cef312483b75ca102e674
Reza <rezamohideen@utexas.edu>
2020-10-05 22:39:32 -0500

    dfs complete

--- P3/src/assignment3/Main.java
+++ P3/src/assignment3/Main.java
@@ -1,12 +1,9 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * Reza Mohideen
+ * rm54783
+ * 16160
  * Slip days used: <0>
  * Git URL:
  * Fall 2020
@@ -23,8 +20,6 @@ public class Main {
     // static variables and constants only here.
     public static Set<String> dictionary;       //all words of provided dictionary
     public static ArrayList<String> wordList;   //user inputs
-    public static String startingWord;          //starting word of word ladder
-    public static String endingWord;            //ending word of world ladder
     public static Scanner kb;                   //input scanner
     public static Hashtable<String, List<String>> comboDict;
 
@@ -42,10 +37,15 @@ public class Main {
             ps = System.out;			// default output to Stdout
         }
         initialize();
+        wordList = parse(kb);
 
         // TODO methods to read in words, output ladder
-        ArrayList<String> wordLadder = getWordLadderBFS(startingWord,endingWord);
+        ArrayList<String> wordLadder = getWordLadderBFS(wordList.get(0),wordList.get(1));
         printLadder(wordLadder);
+
+
+        ArrayList<String> dfs_answer = getWordLadderDFS(wordList.get(0),wordList.get(1));
+        printLadder(dfs_answer);
     }
 
     public static void initialize() {
@@ -53,14 +53,6 @@ public class Main {
         // We will call this method before running our JUNIT tests.  So call it
         // only once at the start of main.
         dictionary = makeDictionary();
-        wordList = parse(kb);
-        if (wordList.get(0) == "/quit"){
-            return;
-        }
-        else {
-            startingWord = wordList.get(0).toString();
-            endingWord = wordList.get(1).toString();
-        }
 
         comboDict = createComboDict(dictionary);
     }
@@ -76,22 +68,66 @@ public class Main {
 
         // check if /quit was entered
         if (userInput.equals("/quit")) {
-            return new ArrayList<String>(Arrays.asList("/quit","/quit"));
+            return new ArrayList<String>();
         }
 
         ArrayList<String> wordList = new ArrayList<String>(Arrays.asList(userInput.split(" ")));
 
-
         return wordList;
     }
 
+    public static ArrayList<String> getAdjacentWords(String word) {
+        ArrayList<String> adjacents = new ArrayList<String>();
+
+        dictionary.remove(word.toUpperCase());
+        for (int i = 0; i < word.length(); i++) {
+            // replace single character with "#"
+            String starWord = word.substring(0, i) + "#" + word.substring(i + 1);
+
+            // get value of dictionary for given key
+            List<String> adjacentWords = comboDict.getOrDefault(starWord.toUpperCase(), new ArrayList<>());
+
+            for (String adjWord : adjacentWords) {
+                if (dictionary.contains(adjWord)) {
+                    adjacents.add(adjWord);
+                    dictionary.remove(adjWord.toUpperCase());
+                }
+            }
+        }
+        return adjacents;
+    }
+
     public static ArrayList<String> getWordLadderDFS(String start, String end) {
+        start = start.toUpperCase();
+        end = end.toUpperCase();
+        ArrayList<String> result = recursiveDFS(start, end, new ArrayList<String>());
 
-        // Returned list should be ordered start to end.  Include start and end.
-        // If ladder is empty, return list with just start and end.
-        // TODO some code
+        if(result.isEmpty()) {
+            result.add(end);
+            result.add(start);
+        }
 
-        return null; // replace this line later with real return
+        Collections.reverse(result);
+        return result;
+    }
+
+    public static ArrayList<String> recursiveDFS(String word, String end, ArrayList<String> ladder) {
+        ArrayList<String> adjacentWords = getAdjacentWords(word.toUpperCase());
+        if (adjacentWords.contains(end.toUpperCase())) {
+            ladder.add(end.toLowerCase());
+            ladder.add(word.toLowerCase());
+            return ladder;
+        }
+
+        for (String adjWord : adjacentWords) {
+             ladder = recursiveDFS(adjWord, end, ladder);
+            if (ladder.size() > 0) {
+                ladder.add(word.toLowerCase());
+                return ladder;
+            }
+        }
+
+        return ladder;
     }
 
     public static class WordNode {
@@ -120,12 +156,13 @@ public class Main {
             WordNode word = Q.remove();
 
             for (int i = 0; i < word.word.length(); i++) {
-                // replace single character with "*"
-                String starWord = word.word.substring(0, i) + "*" + word.word.substring(i + 1);
+                // replace single character with "#"
+                String starWord = word.word.substring(0, i) + "#" + word.word.substring(i + 1);
 
                 // get value of dictionary for given key
                 List<String> adjacentWords = comboDict.get(starWord);
-                adjacentWords = new ArrayList<>(adjacentWords);
+                if (adjacentWords == null) continue;
+                else adjacentWords = new ArrayList<>(adjacentWords);
 
                 // if ending word is found
                 if (adjacentWords.contains(end.toUpperCase())) {
@@ -140,7 +177,7 @@ public class Main {
                     if (!discoveredStrings.contains(adjWord)) {
                         Q.add(new WordNode(adjWord, level+1,word.word));
                         discoveredStrings.add(adjWord);
-                        discoveredNodes.add(new WordNode(adjWord, word.depth, word.word));
+                        discoveredNodes.add(new WordNode(adjWord, word.depth+1, word.word));
                     }
                 }
             }
@@ -149,9 +186,11 @@ public class Main {
 
         if (discoveredStrings.contains(end.toUpperCase())) {
             ladder = backtrackBFS(discoveredNodes);
-            return ladder; // replace this line later with
+            return ladder;
         }
         else {
+            ladder.add(start);
+            ladder.add(end);
             return ladder;
         }
     }
@@ -163,36 +202,28 @@ public class Main {
         String prevWord = "****";
 
         while(prevWord != null) {
-            ladder.add(discoveredNodes.get(index).word);
+            ladder.add(discoveredNodes.get(index).word.toLowerCase());
             prevWord = discoveredNodes.get(index).prev;
             for (int i = 0; i < discoveredNodes.size(); i++) {
                 if (discoveredNodes.get(i).word == prevWord) {
                     index = i;
+                    break;
                 }
             }
         }
+        Collections.reverse(ladder);
         return ladder;
     }
 
-    public static int calcDistance(String adjWord, String endWord) {
-        int distance = 0;
-        for (int i = 0; i < adjWord.length(); i++) {
-            distance += endWord.toUpperCase().charAt(i) - adjWord.charAt(i);
-        }
-
-        return distance;
-    }
-
     public static void printLadder(ArrayList<String> ladder) {
-        if (ladder.size() > 0) {
-            Collections.reverse(ladder);
-            System.out.println("a " + (ladder.size() - 2) + "-rung word ladder exists between " + startingWord + " and " + endingWord);
+        if (ladder.size() > 2) {
+            System.out.println("a " + (ladder.size() - 2) + "-rung word ladder exists between " + ladder.get(0).toLowerCase() + " and " + ladder.get(ladder.size() - 1).toLowerCase());
             for (String word : ladder) {
                 System.out.println(word.toLowerCase());
             }
         }
         else {
-            System.out.println("no word ladder was found between " + startingWord + " and " + endingWord);
+            System.out.println("no word ladder can be found between " + ladder.get(0).toLowerCase() + " and " + ladder.get(1).toLowerCase());
         }
     }
 
@@ -225,8 +256,8 @@ public class Main {
         for (String word : wordList) {
             // iterate over each character in word
             for (int i = 0; i < word.length(); i++){
-                // replace single character with "*"
-                String starWord = word.substring(0,i) + "*" + word.substring(i+1);
+                // replace single character with "#"
+                String starWord = word.substring(0,i) + "#" + word.substring(i+1);
 
                 // if dictionary is empty or key does not exist add key,value
                 if (comboDict.isEmpty() == true || comboDict.get(starWord) == null) {
